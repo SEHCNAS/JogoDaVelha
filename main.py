0fc0bb57af8f723164ca37b145defb2767e1bed5
Gabriel <thebug@outlook.com.br>
2023-06-22 10:14:45 -0300

    Adicionado exemplo de como jogar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def PrintJogo(vetor):
+def printJogo(vetor):
     print(vetor[0] + '|' + vetor[1] + '|' + vetor[2])
     print('-----')
     print(vetor[3] + '|' + vetor[4] + '|' + vetor[5])
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 
     ContinuaJogo = True
     IdentificadorJogadorDoisValido = False
+    vetorExemplo = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     vetor = [' '] * 9
 
     TiposJogadas = ['', '']
@@ -22,61 +23,65 @@ if __name__ == '__main__':
         if TiposJogadas[1] != TiposJogadas[0]:
             IdentificadorJogadorDoisValido = True
 
-    PrintJogo(vetor)
+    print('Use o teclado numerico para escolher onde quer jogar.')
+    printJogo(vetorExemplo)
 
     while ContinuaJogo:
 
-        posicao = int(input("Digite a casa em que deseja jogar: "))
+        posicao = input("Digite a casa em que deseja jogar: ")
+        if posicao.isnumeric():
+            posicaoNum = int(posicao)
+            if vetor[posicaoNum] == ' ':
 
-        if vetor[posicao] == ' ':
+                Jogada += 1
+                if Jogada % 2 == 1:
+                    vetor[posicaoNum] = TiposJogadas[0]
+                else:
+                    vetor[posicaoNum] = TiposJogadas[1]
 
-            Jogada += 1
-            if Jogada % 2 == 1:
-                vetor[posicao] = TiposJogadas[0]
             else:
-                vetor[posicao] = TiposJogadas[1]
+                print('Ja existe valor nesse campo, jogue em outro casa!')
 
+            if Jogada >= 5:
+                for i in range(2):
+                    # vencer na linha
+                    if vetor[0] == TiposJogadas[i] and vetor[1] == TiposJogadas[i] and vetor[2] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    if vetor[3] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[5] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    if vetor[6] == TiposJogadas[i] and vetor[7] == TiposJogadas[i] and vetor[8] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    # Vencer na coluna
+                    if vetor[0] == TiposJogadas[i] and vetor[3] == TiposJogadas[i] and vetor[6] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    if vetor[1] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[7] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    if vetor[2] == TiposJogadas[i] and vetor[5] == TiposJogadas[i] and vetor[8] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    # Vence em X
+                    if vetor[2] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[6] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+                    if vetor[0] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[8] == TiposJogadas[i]:
+                        print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
+                        ContinuaJogo = False
+
+            printJogo(vetor)
+
+            if Jogada == 9:
+                ContinuaJogo = False
         else:
-            print('Ja existe valor nesse campo, jogue em outro casa!')
-
-        if Jogada >= 5:
-            for i in range(2):
-                # vencer na linha
-                if vetor[0] == TiposJogadas[i] and vetor[1] == TiposJogadas[i] and vetor[2] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                if vetor[3] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[5] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                if vetor[6] == TiposJogadas[i] and vetor[7] == TiposJogadas[i] and vetor[8] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                # Vencer na coluna
-                if vetor[0] == TiposJogadas[i] and vetor[3] == TiposJogadas[i] and vetor[6] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                if vetor[1] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[7] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                if vetor[2] == TiposJogadas[i] and vetor[5] == TiposJogadas[i] and vetor[8] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                # Vence em X
-                if vetor[2] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[6] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-                if vetor[0] == TiposJogadas[i] and vetor[4] == TiposJogadas[i] and vetor[8] == TiposJogadas[i]:
-                    print('O {Jogador}º jogador venceu'.format(Jogador=(i + 1)))
-                    ContinuaJogo = False
-
-        PrintJogo(vetor)
-
-        if Jogada == 9:
-            ContinuaJogo = False
+            print('O valor digitado não é um numero valido. \n Escolha um numero de 0 a 8.')
